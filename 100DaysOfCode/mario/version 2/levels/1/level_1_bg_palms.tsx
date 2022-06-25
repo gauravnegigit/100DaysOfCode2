98c951cbafd66d97cd4fcb8920263a2399902b11
Gaurav Negi <97227844+gauravnegigit@users.noreply.github.com>
2022-06-25 14:07:09 +0530

    Add files via upload

---
+++ 100DaysOfCode/mario/version 2/levels/1/level_1_bg_palms.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="level_1_bg_palms" tilewidth="64" tileheight="64" tilecount="4" columns="2">
+ <image source="../../graphics/terrain/palm_bg/bg_palm_1.png" width="128" height="128"/>
+</tileset>
